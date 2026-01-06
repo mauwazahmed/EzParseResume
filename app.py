3a0ec35fea2832603a37b128d712a11766b08e21
mauwazahmed <56031816+mauwazahmed@users.noreply.github.com>
2026-01-06 21:59:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ def parse_resume_with_openai(raw_text: str) -> dict:
         thread_id=thread.id,
         assistant_id=ASSISTANT_ID
     )
-    messages = client.beta.threads.messages.list(order="desc", limit=1,thread_id=thread.id)
-    reply = messages[-1].content
+    messages = client.beta.threads.messages.list(order="desc", limit=1,thread_id=thread.id,run_id=run.id)
+    reply = messages.data[0].content[0].text
 
     return json.loads(reply)
 # ---------------- UI ----------------
@@ -257,6 +257,7 @@ with pikepdf.open("resume.pdf") as pdf:
   ]
 }
 ''')
+
 
 
 
